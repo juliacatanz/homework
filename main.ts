4a7f8045c12d4b51f45ac96ebf9380e495096fa2
juliacatanz <113040686+juliacatanz@users.noreply.github.com>
2022-09-15 11:07:15 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let _of_people = 0
+input.onButtonPressed(Button.A, function () {
+    _of_people += 1
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    _of_people += -1
+})
+basic.forever(function () {
+	
+})
